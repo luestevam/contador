a12a3f5c9b6bbb35a6493731d7558a98e13b01d4
luestevam <luestevam@hotmail.com>
2021-04-06 17:12:53 -0300

    Insert units, sum and less

--- lib/main.dart
+++ lib/main.dart
@@ -3,27 +3,82 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
       title: "Contador",
-      home: Column(
-        mainAxisAlignment:
-            MainAxisAlignment.center, //para alinhar com eixo central
-        //usei column para colocar os widget em pilha
+      //exemplos do widget no flutter https://flutter.dev/docs/development/ui/widgets/layout
+      home: Home()));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+//Minhas váriaveis
+  int _unit = 0;
+  void _changeUnit(int delta) {
+    setState(() {
+      _unit += delta;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Contador"),
+        backgroundColor: Colors.amberAccent,
+        centerTitle: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Text(
-            "Pessoas: 0",
-            style: TextStyle(
-                color: Colors.blueAccent, fontWeight: FontWeight.bold),
-          ),
-          FlatButton(
-            child: Text(
-              "+1",
-              style: TextStyle(fontSize: 40.0, color: Colors.white),
-            ),
-          ),
-          Text(
-            "Pode Entrar",
-            style: TextStyle(
-                color: Colors.blueAccent, fontWeight: FontWeight.bold),
+          Column(
+            children: <Widget>[
+              Text(
+                "Unidade: $_unit",
+                style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.blueAccent,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: FlatButton(
+                      child: Text(
+                        "ADD+",
+                        style:
+                            TextStyle(fontSize: 30.0, color: Colors.blueAccent),
+                      ),
+                      onPressed: () {
+                        _changeUnit(1);
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: FlatButton(
+                      child: Text(
+                        "LEES-",
+                        style:
+                            TextStyle(fontSize: 30.0, color: Colors.blueAccent),
+                      ),
+                      onPressed: () {
+                        _changeUnit(-1);
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ],
           )
         ],
-      )));
+      ),
+    );
+  }
 }
